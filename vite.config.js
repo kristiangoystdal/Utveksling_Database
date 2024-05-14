c2daac7128963da466e45e7f453643cba2805ad9
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-14 23:11:34 +0200

    Remove vite-plugin-raw.js and fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
-import rawPlugin from './vite-plugin-raw'; // Ensure this path is correct
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    rawPlugin() // Register the custom plugin
   ],
   resolve: {
     alias: {
