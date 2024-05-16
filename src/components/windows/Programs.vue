53b3e980fb8c25bd491f07a1945bd968cff75351
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-16 15:17:42 +0200

    refactor: Add lazy loading for images in Programs page

--- src/components/windows/Programs.vue
+++ src/components/windows/Programs.vue
@@ -3,29 +3,80 @@
 		<h2>Programs</h2>
 		<p>Welcome to the Programs page!</p>
 		<v-btn @click="addTestExchange">Add test exchange</v-btn>
+
+		<v-data-table
+			v-model:expanded="expanded"
+			:headers="headers"
+			:items="exchangeList"
+			item-value="university"
+			show-expand
+			class="elevation-1"
+		>
+			<template v-slot:expanded-row="{ columns, item }">
+				<tr>
+					<td :colspan="columns.length * 0.9">
+						{{ item.courses.Høst }}
+					</td>
+					<br />
+					<!-- <td :colspan="columns.length * 0.9">
+						{{ item.courses.Vår }}
+					</td> -->
+				</tr>
+			</template>
+		</v-data-table>
 	</div>
 </template>
 
 <script>
 import { auth, db } from "../../js/firebaseConfig";
-import { ref as dbRef, set, get } from "firebase/database";
+import { ref as dbRef, set, get, child } from "firebase/database";
 
 export default {
 	data() {
-		return {};
+		return {
+			expanded: [],
+			headers: [
+				{ title: "Universitet", align: "start", key: "university" },
+				{ title: "Land", align: "end", key: "country" },
+				{ title: "Studieår", align: "end", key: "studyYear" },
+				{ title: "Studie", align: "end", key: "study" },
+				{ title: "Spesialisering", align: "end", key: "specialization" },
+				{ title: "Antall Semestere ", align: "end", key: "numSemesters" },
+			],
+			exchangeList: [],
+		};
+	},
+	created() {
+		this.fetchExchangeData();
 	},
 	methods: {
+		async fetchExchangeData() {
+			try {
+				const snapshot = await get(child(dbRef(db), `exchanges`));
+				if (snapshot.exists()) {
+					const exchanges = snapshot.val();
+					this.exchangeList = Object.values(exchanges).map((exchange) => ({
+						...exchange,
+					}));
+				} else {
+					console.log("No data available");
+				}
+			} catch (error) {
+				console.error("Error fetching exchange data: ", error);
+			}
+		},
 		async addTestExchange() {
 			const user = auth.currentUser;
 			if (user) {
 				console.log("Adding test exchange...");
 				const exchangeData = {
-					university: "University of California, Berkeley",
-					country: "USA",
-					studyYear: 3,
+					university: "University of Lisbon",
+					country: "Portugal",
+					studyYear: 4,
+					numSemesters: 1,
 					courses: {
-						Høst: {
-							1: {
+						Høst: [
+							{
 								exchangeID: user.uid,
 								year: 2018,
 								courseCode: "CS101",
@@ -36,7 +87,7 @@ export default {
 								ETCSPoints: 7.5,
 								comments: "Veldig bra fag!",
 							},
-							2: {
+							{
 								exchangeID: user.uid,
 								year: 2018,
 								courseCode: "c2",
@@ -47,36 +98,14 @@ export default {
 								ETCSPoints: 7.5,
 								comments: "Veldig bra fag!",
 							},
-						},
-						Vår: {
-							1: {
-								exchangeID: user.uid,
-								year: 2019,
-								courseCode: "CS101",
-								courseName: "Spansk",
-								replacedCourseCode: "384",
-								replacedCourseName: "Objektorientert programmering",
-								institute: "Department of Computer Science",
-								ETCSPoints: 6,
-								comments: "Vanskelig",
-							},
-							2: {
-								exchangeID: user.uid,
-								year: 2019,
-								courseCode: "c15",
-								courseName: "Course 15",
-								replacedCourseCode: "INF1010",
-								replacedCourseName: "Objektorientert programmering",
-								institute: "Department of Computer Science",
-								ETCSPoints: 7.5,
-								comments: "Supert!",
-							},
-						},
+						],
+						Vår: [],
 					},
 				};
 				try {
 					await set(dbRef(db, `exchanges/${user.uid}`), exchangeData);
 					console.log("Test exchange added!");
+					this.fetchExchangeData(); // Refresh the data
 				} catch (error) {
 					console.error("Error adding exchange: ", error);
 				}
@@ -85,6 +114,5 @@ export default {
 			}
 		},
 	},
-	mounted() {},
 };
 </script>
