4191a959a0d95b8c8ca2c1df6ca01ff001b77ee0
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-16 22:10:52 +0200

    Add county filter and a virtual data table for the courses for each exchange entry

--- src/components/windows/Programs.vue
+++ src/components/windows/Programs.vue
@@ -3,7 +3,36 @@
 		<h2>Programs</h2>
 		<p>Welcome to the Programs page!</p>
 		<v-btn @click="addTestExchange">Add test exchange</v-btn>
+	</div>
+	<div>
+		<v-autocomplete
+			v-model="countryValues"
+			:items="countryList"
+			label="Land"
+			multiple
+			chips
+			clearable
+			:search-input.sync="countrySearch"
+		>
+			<template v-slot:selection="{ item, index }">
+				<v-chip v-if="index < 2" close @click:close="remove(item)">
+					<span>{{ item }}</span>
+				</v-chip>
+				<span
+					v-if="index === 2"
+					class="text-grey text-caption align-self-center"
+				>
+					(+{{ countryValues.length - 2 }} others)
+				</span>
+			</template>
+		</v-autocomplete>
 
+		{{ countryValues.length >= 2 }}
+	</div>
+	<div>
+		<v-btn @click="fetchExchangeData">Oppdater</v-btn>
+	</div>
+	<div>
 		<v-data-table
 			v-model:expanded="expanded"
 			:headers="headers"
@@ -11,16 +40,36 @@
 			item-value="university"
 			show-expand
 			class="elevation-1"
+			id="main-table-width"
 		>
 			<template v-slot:expanded-row="{ columns, item }">
 				<tr>
-					<td :colspan="columns.length * 0.9">
-						{{ item.courses.Høst }}
+					<td :colspan="columns.length">
+						<div>
+							<br />
+							<h3>Fag Høst</h3>
+
+							<v-data-table-virtual
+								:headers="headersCourses"
+								:items="item.courses.Høst"
+								item-value="name"
+								dense
+							></v-data-table-virtual>
+							<br />
+							<h3>Fag Vår</h3>
+
+							<v-data-table-virtual
+								:headers="headersCourses"
+								:items="item.courses.Vår"
+								item-value="name"
+								dense
+							></v-data-table-virtual>
+							<br />
+						</div>
+						<!-- {{ item.courses.Høst }}
+						<br /><br />
+						{{ item.courses.Vår }} -->
 					</td>
-					<br />
-					<!-- <td :colspan="columns.length * 0.9">
-						{{ item.courses.Vår }}
-					</td> -->
 				</tr>
 			</template>
 		</v-data-table>
@@ -29,7 +78,15 @@
 
 <script>
 import { auth, db } from "../../js/firebaseConfig";
-import { ref as dbRef, set, get, child } from "firebase/database";
+import {
+	ref as dbRef,
+	set,
+	get,
+	child,
+	query,
+	orderByChild,
+	equalTo,
+} from "firebase/database";
 
 export default {
 	data() {
@@ -43,26 +100,138 @@ export default {
 				{ title: "Spesialisering", align: "end", key: "specialization" },
 				{ title: "Antall Semestere ", align: "end", key: "numSemesters" },
 			],
+			headersCourses: [
+				{ title: "Course Name", align: "start", key: "courseName" },
+				{ title: "Course Code", align: "end", key: "courseCode" },
+				{
+					title: "Replaced Course Name",
+					align: "end",
+					key: "replacedCourseName",
+				},
+				{
+					title: "Replaced Course Code",
+					align: "end",
+					key: "replacedCourseCode",
+				},
+				{ title: "Institute", align: "end", key: "institute" },
+				{ title: "ETCS Points", align: "end", key: "ETCSPoints" },
+				{ title: "Comments", align: "end", key: "comments" },
+			],
 			exchangeList: [],
+			countryList: [
+				"Spain",
+				"Portugal",
+				"France",
+				"Germany",
+				"Italy",
+				"Sweden",
+				"Norway",
+				"Denmark",
+				"Finland",
+				"Iceland",
+				"United Kingdom",
+				"Ireland",
+				"Netherlands",
+				"Belgium",
+				"Luxembourg",
+				"Switzerland",
+				"Austria",
+				"Czech Republic",
+				"Slovakia",
+				"Hungary",
+				"Slovenia",
+				"Croatia",
+				"Bosnia and Herzegovina",
+				"Serbia",
+				"Montenegro",
+				"Albania",
+				"North Macedonia",
+				"Kosovo",
+				"Greece",
+				"Bulgaria",
+				"Romania",
+				"Moldova",
+				"Ukraine",
+				"Belarus",
+				"Russia",
+				"Poland",
+				"Lithuania",
+				"Latvia",
+				"Estonia",
+				"Turkey",
+				"Cyprus",
+				"Malta",
+				"Andorra",
+				"Monaco",
+				"San Marino",
+				"Vatican City",
+				"Liechtenstein",
+			],
+			countryValues: ["Spain"],
+			countrySearch: "",
 		};
 	},
 	created() {
 		this.fetchExchangeData();
 	},
+	computed: {
+		filteredCountryList() {
+			if (!this.countrySearch) {
+				return this.countryList;
+			}
+			return this.countryList.filter((country) =>
+				country.toLowerCase().includes(this.countrySearch.toLowerCase())
+			);
+		},
+	},
 	methods: {
-		async fetchExchangeData() {
-			try {
-				const snapshot = await get(child(dbRef(db), `exchanges`));
-				if (snapshot.exists()) {
-					const exchanges = snapshot.val();
-					this.exchangeList = Object.values(exchanges).map((exchange) => ({
-						...exchange,
-					}));
-				} else {
-					console.log("No data available");
+		remove(item) {
+			this.countryValues = this.countryValues.filter((i) => i !== item);
+		},
+		async FetchDifferentValues(path, list) {
+			this.exchangeList = [];
+			for (const country of list) {
+				console.log(country);
+				try {
+					const exchangesRef = dbRef(db, "exchanges"); // Use the provided path
+					const q = query(exchangesRef, orderByChild(path), equalTo(country));
+
+					const snapshot = await get(q);
+
+					if (snapshot.exists()) {
+						const exchanges = snapshot.val();
+						this.exchangeList = this.exchangeList.concat(
+							Object.values(exchanges).map((exchange) => ({
+								...exchange,
+							}))
+						);
+					} else {
+						console.log(`No data available for country: ${country}`);
+					}
+				} catch (error) {
+					console.error("Error fetching exchange data: ", error);
 				}
-			} catch (error) {
-				console.error("Error fetching exchange data: ", error);
+			}
+		},
+		async fetchExchangeData() {
+			if (this.countryValues.length > 0) {
+				console.log("Fetching exchange data with country filter...");
+				this.FetchDifferentValues("country", this.countryValues);
+			} else {
+				console.log("Fetching exchange data...");
+				try {
+					const snapshot = await get(child(dbRef(db), `exchanges`));
+					if (snapshot.exists()) {
+						const exchanges = snapshot.val();
+						this.exchangeList = Object.values(exchanges).map((exchange) => ({
+							...exchange,
+						}));
+					} else {
+						console.log("No data available");
+					}
+				} catch (error) {
+					console.error("Error fetching exchange data: ", error);
+				}
 			}
 		},
 		async addTestExchange() {
@@ -70,10 +239,12 @@ export default {
 			if (user) {
 				console.log("Adding test exchange...");
 				const exchangeData = {
-					university: "University of Lisbon",
-					country: "Portugal",
+					university: "University of Madrid",
+					country: "Spain",
 					studyYear: 4,
-					numSemesters: 1,
+					numSemesters: 2,
+					study: "user.study",
+					specialization: "user.specialization",
 					courses: {
 						Høst: [
 							{
@@ -99,7 +270,41 @@ export default {
 								comments: "Veldig bra fag!",
 							},
 						],
-						Vår: [],
+						Vår: [
+							{
+								exchangeID: user.uid,
+								year: 2018,
+								courseCode: "c2",
+								courseName: "Course 32",
+								replacedCourseCode: "INF1010",
+								replacedCourseName: "Objektorientert programmering",
+								institute: "Department of Computer Science",
+								ETCSPoints: 7.5,
+								comments: "Veldig bra fag!",
+							},
+							{
+								exchangeID: user.uid,
+								year: 2018,
+								courseCode: "c2",
+								courseName: "Course 47",
+								replacedCourseCode: "INF1010",
+								replacedCourseName: "Objektorientert programmering",
+								institute: "Department of Computer Science",
+								ETCSPoints: 75,
+								comments: "Veldig bra fag!",
+							},
+							{
+								exchangeID: user.uid,
+								year: 2018,
+								courseCode: "c2",
+								courseName: "Course 5",
+								replacedCourseCode: "INF1010",
+								replacedCourseName: "Objektorientert programmering",
+								institute: "Department of Computer Science",
+								ETCSPoints: 5,
+								comments: "Veldig!",
+							},
+						],
 					},
 				};
 				try {
@@ -116,3 +321,21 @@ export default {
 	},
 };
 </script>
+
+<style scoped>
+#main-table-width {
+	margin: auto;
+	width: 90% !important;
+	max-width: 1200px !important;
+	min-width: 800px !important;
+}
+.text-grey {
+	color: grey;
+}
+.text-caption {
+	font-size: 0.75rem;
+}
+.align-self-center {
+	align-self: center;
+}
+</style>
