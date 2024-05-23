5c4cc5d5885e90718a3b5182eaf9df6ee1da74ec
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-24 00:29:11 +0200

    Remove unnecessary code related to language selection

--- src/components/windows/Home.vue
+++ src/components/windows/Home.vue
@@ -1,20 +1,11 @@
 <template>
 	<div>
 		<h2>Home</h2>
-		<p>{{ $t("welcome") }}</p>
-		<p>{{ $t("goodbye") }}</p>
-		<v-btn @click="changeLanguage('en')"> English </v-btn>
-		<v-btn @click="changeLanguage('no')"> Norwegian </v-btn>
 	</div>
 </template>
 
 <script>
 export default {
-	methods: {
-		changeLanguage(lang) {
-			this.$i18n.locale = lang;
-			localStorage.setItem("language", lang); // Save the selected language to localStorage
-		},
-	},
+	
 };
 </script>
