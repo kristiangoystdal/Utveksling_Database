1ef48efa772d703324471386b8bc22a0042e665a
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-26 23:06:56 +0200

    Refactor CreditsTable.vue component to use JSON data

--- src/components/CreditsTable.vue
+++ src/components/CreditsTable.vue
@@ -1,108 +1,128 @@
 <template>
 	<div>
-		<h1>{{ title }}</h1>
+		<h1>JSON Data</h1>
 		<v-data-table
 			v-model:expanded="expanded"
 			:headers="headers"
-			:items="countries"
+			:items="tableData"
 			item-value="country"
 			show-expand
+			class="elevation-1"
 		>
-			<template v-slot:[`item.country`]="{ item }">
-				{{ item.country }}
-			</template>
-			<template v-slot:[`expanded-item`]="{ item }">
-				hei
-				<!-- <v-data-table
-					:headers="nestedHeaders"
-					:items="item.universities"
-					class="nested-table"
-					hide-default-footer
-				>
-					<template v-slot:[`item.name`]="{ item }">
-						{{ item.name }}
-					</template>
-					<template v-slot:[`item.field`]="{ item }">
-						{{ item.field }}
-					</template>
-					<template v-slot:[`item.bachelor`]="{ item }">
-						{{ item.bachelor }}
-					</template>
-					<template v-slot:[`item.master`]="{ item }">
-						{{ item.master }}
-					</template>
-					<template v-slot:[`item.comments`]="{ item }">
-						{{ item.comments }}
-					</template>
-				</v-data-table> -->
+			<template v-slot:expanded-row="{ columns, item }">
+				<tr>
+					<td :colspan="columns.length" id="coursesStyle">
+						<div>
+							<br />
+							<h3 v-if="item.universities && item.universities.length">
+								Universities
+							</h3>
+							<v-data-table-virtual
+								v-if="item.universities && item.universities.length"
+								:headers="nestedHeaders"
+								:items="item.universities"
+								item-value="name"
+								dense
+								class="virtual-table"
+								hide-default-footer
+							>
+								<template v-slot:[`item.name`]="{ item }">
+									{{ item.name }}
+								</template>
+								<template v-slot:[`item.field`]="{ item }">
+									{{ item.field || "N/A" }}
+								</template>
+								<template v-slot:[`item.bachelorCredits`]="{ item }">
+									{{ item.bachelorCredits || "N/A" }}
+								</template>
+								<template v-slot:[`item.masterCredits`]="{ item }">
+									{{ item.masterCredits || "N/A" }}
+								</template>
+								<template v-slot:[`item.comments`]="{ item }">
+									<div>
+										<v-icon small class="mr-2" @click="showComments(item)">
+											mdi-comment
+										</v-icon>
+									</div>
+								</template>
+							</v-data-table-virtual>
+							<br />
+						</div>
+					</td>
+				</tr>
 			</template>
 		</v-data-table>
+
+		<!-- Comment Dialog -->
+		<v-dialog v-model="commentDialog" max-width="500px">
+			<v-card>
+				<v-card-title>{{ currentUniversityName }}</v-card-title>
+				<v-card-text>{{ currentComments }}</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn color="blue darken-1" text @click="closeCommentDialog">
+						Close
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</div>
 </template>
 
 <script>
-import creditsData from "../data/credits.json";
+// Import the JSON data
+import jsonData from "../data/credits.json";
 
 export default {
 	data() {
 		return {
-			title: "Credits Table",
-			headers: [{ title: "Country", value: "country" }],
+			expanded: [],
+			headers: [
+				{ title: "Country", value: "country" },
+				{ title: "University Count", value: "universityCount" },
+			],
 			nestedHeaders: [
-				{ title: "Institution", value: "name" },
+				{ title: "University", value: "name" },
 				{ title: "Field", value: "field" },
-				{ title: "Bachelor Credits", value: "bachelor" },
-				{ title: "Master Credits", value: "master" },
+				{ title: "Bachelor Credits", value: "bachelorCredits" },
+				{ title: "Master Credits", value: "masterCredits" },
 				{ title: "Comments", value: "comments" },
 			],
-			countries: [],
-			expanded: [],
+			tableData: this.formatData(jsonData),
+			commentDialog: false,
+			currentUniversityName: "",
+			currentComments: "",
 		};
 	},
 	methods: {
-		fetchData() {
-			const data = [];
-
-			Object.entries(creditsData).forEach(([country, universities]) => {
-				const universityData = [];
-				Object.entries(universities).forEach(([university, details]) => {
-					Object.entries(details).forEach(([field, info]) => {
-						if (
-							info.credits &&
-							typeof info.credits.bachelor !== "undefined" &&
-							typeof info.credits.master !== "undefined"
-						) {
-							universityData.push({
-								name: university,
-								field: field,
-								bachelor: info.credits.bachelor,
-								master: info.credits.master,
-								comments: info.comments || "",
-							});
-						}
-					});
-				});
-				data.push({
-					country: country,
-					universities: universityData,
-				});
-			});
-
-			this.countries = data;
-			console.log(this.countries); // Debugging: Check the structure of the data
+		formatData(data) {
+			return Object.entries(data).map(([country, universities]) => ({
+				country,
+				universityCount: Object.keys(universities).length,
+				universities: Object.entries(universities).flatMap(
+					([university, details]) =>
+						details.map((detail) => ({
+							name: university,
+							...detail,
+						}))
+				),
+			}));
 		},
-	},
-	created() {
-		this.fetchData();
+		showComments(university) {
+			this.currentUniversityName = university.name;
+			this.currentComments = university.comments || "No comments available";
+			this.commentDialog = true;
+		},
+		closeCommentDialog() {
+			this.commentDialog = false;
+		},
 	},
 };
 </script>
 
 <style scoped>
-h1 {
-	color: blue;
-}
-.nested-table {
-	margin: 0 20px;
+.virtual-table {
+	max-height: 300px;
+	overflow-y: auto;
 }
 </style>
