cf7d5c9e4caf4024b049aea693cd43e7ef9fb610
Kristian Goystdal <krisgoy100@gmail.com>
2024-08-10 00:27:53 +0200

    refactor: Add confirmation dialog for leaving the EditExchange page

--- src/js/router.js
+++ src/js/router.js
@@ -10,11 +10,13 @@ import Login from '../components/profile/Login.vue';
 import NotFound from '../components/error/NotFound.vue';
 
 import store from './store.js'; 
+import { translate } from './i18n';
+
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/utvekslinger', name: 'Exchanges', component: Exchanges },
-  { path: '/min_utveksling', name: 'EditExchange', component: EditExchange },
+  { path: '/min_utveksling', name: 'EditExchange', component: EditExchange, meta: { needComfirmation: true } },
   { path: '/kontakt', name: 'Contact', component: Contact },
   { path: '/profil', name: 'Account', component: Account, meta: { requiresAuth: true } },
   { path: '/logg_inn', name: 'Login', component: Login },
@@ -26,14 +28,29 @@ const router = createRouter({
   routes,
 });
 
-// Navigation guard for authentication
 router.beforeEach((to, from, next) => {
+  
+
+  // Check if the route requires confirmation
+  if (from.matched.some(record => record.meta.needComfirmation)) {
+    if (from.query.edited === 'true') {
+
+      if (!confirm(translate('operations.confirmLeavePage'))) {
+        // If user cancels, do not navigate further
+        return next(false);
+      }
+    }
+  }
+  
+  // Navigation guard for authentication
   const isAuthenticated = store.getters.isAuthenticated;
   if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated) {
-    next({ name: 'Login' });
-  } else {
-    next();
+    // Redirect to the login page if not authenticated
+    return next({ name: 'Login' });
   }
+
+  next();
 });
+
 
 export default router;
