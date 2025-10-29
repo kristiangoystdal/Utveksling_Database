0c5742ef61293a0e8f5c129ce1ec892296dda469
Kristian Goystdal <krisgoy100@gmail.com>
2025-10-29 23:43:39 +0100

    Add Confirmation component with dialog functionality and event emissions

---
+++ src/components/common/Confirmation.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-text>
+        <p>{{ message }}</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="btn btn-primary" @click="emitYes">Yes</v-btn>
+        <v-btn class="btn btn-red" @click="emitNo">No</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "Confirmation",
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dialog: this.value,
+      inputText: "",
+    };
+  },
+  watch: {
+    value(val) {
+      this.dialog = val;
+    },
+    dialog(val) {
+      this.$emit("input", val);
+    },
+  },
+  methods: {
+    emitYes() {
+      this.$emit("yes", this.inputText);
+      this.dialog = false;
+    },
+    emitNo() {
+      this.$emit("no");
+      this.dialog = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add any custom styles here */
+</style>
