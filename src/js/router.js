afc96a58c76ab0b4b1d4589279af39d3cf34729a
Kristian Gøystdal <krisgoy100@gmail.com>
2025-11-26 22:55:09 +0100

    fix: add scroll behavior to router configuration

--- src/js/router.js
+++ src/js/router.js
@@ -32,6 +32,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  }
 });
 
 router.beforeEach((to, from, next) => {
